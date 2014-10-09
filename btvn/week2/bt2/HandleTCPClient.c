b5275494d70caaf99f3be73271c53ecce99576b5
AnonyMous <nguyentuananh1993@gmail.com>
2014-10-09 20:39:31 +0700

    done code week 2

---
+++ btvn/week2/bt2/HandleTCPClient.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#define RCVBUFSIZE 32
+
+void HandleTCPClient(int clntSocket){
+	char echoBuffer[RCVBUFSIZE];	// buffer for echo string
+	int recvMsgSize;				// size of received message
+
+	//recieve message from client
+	if((recvMsgSize = recv(clntSocket, echoBuffer, RCVBUFSIZE, 0))<0)
+		DieWithError("recv() failed");
+	// send recieved string and receive again until end of transmission
+	while(recvMsgSize > 0){
+		//echo message back to client
+		int i=0;
+		for(i=0;i<strlen(echoBuffer);i++){
+			echoBuffer[i]=tolower(echoBuffer[i]);
+		}
+		if(send(clntSocket,echoBuffer,recvMsgSize,0)!= recvMsgSize)
+			DieWithError("send() failed");
+
+		// see if there is more data to receive
+		if((recvMsgSize = recv(clntSocket,echoBuffer,RCVBUFSIZE,0))<0)
+			DieWithError("recv() failed");
+	}
+	close(clntSocket);
+}
